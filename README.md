890c18803a91a0c90ce71c049aee8b3ade82f506
DavidSchott <daschott@iastate.edu>
2016-09-25 15:22:38 -0500

    Drag & drop working now

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# portfolio1
+# Portfolio1
 
-### API's
+### Requirements
 * [WebTorrent API](https://webtorrent.io/docs)
 * [Instant IO](https://github.com/feross/instant.io)
 * [drag-drop](https://www.npmjs.com/package/drag-drop)
--- form.js
+++ form.js
@@ -6,16 +6,34 @@ function setTorrentLink(){
     return checkHTMLStorage(setSessionStorage,'torrent-dl',torrentLink)
 }
 
-var dragDrop = new dragDrop();
-// When user drops files on the browser, create a new torrent and start seeding it!
-dragDrop('#dropbox', function (files) {
-    alert(files);
-    client.seed(files, function (torrent) {
-        console.log('Client is seeding:', torrent.infoHash)
-    });
-});
+var client = new WebTorrent();
+function handleFileSelect(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
 
-function seedFiles(){
-    alert("Called seedFiles");
-    return false;
+    var files = evt.dataTransfer.files; // FileList object.
+    client.seed(files, function (torrent) {
+        console.log('Client is seeding:', torrent.infoHash);
+        var output = [];
+        for (var i = 0, f; f = files[i]; i++) {
+            output.push('<li>Seeding: <strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
+                f.size, ' bytes',
+                '</li>');
+        }
+        document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
+        document.getElementById('uri').href = torrent.magnetURI;
+        document.getElementById('uri').style.display = "inline-block";
+    });
+    // files is a FileList of File objects. List some properties.
 }
+
+function handleDragOver(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+}
+
+// Setup the dnd listeners.
+var dropZone = document.getElementById('drop_zone');
+dropZone.addEventListener('dragover', handleDragOver, false);
+dropZone.addEventListener('drop', handleFileSelect, false);
