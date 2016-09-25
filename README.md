2a4729143f7ad85f89fb8e5e6ccefb6974cbe710
DavidSchott <daschott@iastate.edu>
2016-09-25 13:47:19 -0500

    Added basic layout for drag&drop, though its not working...

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ### API's
 * [WebTorrent API](https://webtorrent.io/docs)
 * [Instant IO](https://github.com/feross/instant.io)
+* [drag-drop](https://www.npmjs.com/package/drag-drop)
--- form.js
+++ form.js
@@ -5,3 +5,17 @@ function setTorrentLink(){
     var torrentLink = document.forms["download-form"]["torrentlink"].value;
     return checkHTMLStorage(setSessionStorage,'torrent-dl',torrentLink)
 }
+
+var dragDrop = new dragDrop();
+// When user drops files on the browser, create a new torrent and start seeding it!
+dragDrop('#dropbox', function (files) {
+    alert(files);
+    client.seed(files, function (torrent) {
+        console.log('Client is seeding:', torrent.infoHash)
+    });
+});
+
+function seedFiles(){
+    alert("Called seedFiles");
+    return false;
+}
